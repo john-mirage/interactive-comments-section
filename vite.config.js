13bc9f6699d96a641c80a916012b332eebcf8450
john-mirage <chaosdynamix@posteo.net>
2022-05-23 17:54:40 +0200

    feat: initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/interactive-comments-section/",
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, 'src/assets'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@data': resolve(__dirname, 'src/data'),
+    },
+  },
+});
